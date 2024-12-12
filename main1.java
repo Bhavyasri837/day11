ca105de37861d3eb66c6b01a4ecde4b49708435a
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-12 13:38:59 +0530

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+interface printable
+{
+}
+class  h implements printable{
+    public void print()
+    {
+        System.out.println("hello students");
+
+    }
+}
+public class main1{
+    public static void main(String args[])
+    {
+        h f=new h();
+        f.print();
+    }
+}
