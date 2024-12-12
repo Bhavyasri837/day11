8934834aa6ab8c76d98fde9d1a74b996879c8a48
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-12 15:01:08 +0530

    Add files via upload

---
+++ main5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class main5 {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(3);
+        a.add(35);
+        a.add(67);
+        a.add(56);
+        a.add(96);
+        a.add(34);
+        a.add(64);
+        System.out.println(a);
+        
+    }
+    
+}
